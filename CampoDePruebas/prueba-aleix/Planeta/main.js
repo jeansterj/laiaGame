a477d65cd43b20cb4f61034a483749c38f4e0791
AleixCorres <145031923+AleixCorres@users.noreply.github.com>
2023-11-14 18:44:38 +0100

    Add files via upload

---
+++ CampoDePruebas/prueba-aleix/Planeta/main.js
@@ -0,0 +1,38 @@
+<<<<<<< Updated upstream
+addEventListener("DOMContentLoaded", (event) => {
+
+
+    const wrapper = document.querySelector('.wrapper');
+    const loginLink = document.querySelector('.loginLink');
+    const registerLink = document.querySelector('.registerLink');
+    const btnPopup = document.querySelector('.btnLogin-popup');
+    const iconClose = document.querySelector('.iconClose');
+    
+    
+    registerLink.addEventListener('click', ()=> {wrapper.classList.add('active');});
+    
+    loginLink.addEventListener('click', ()=> {wrapper.classList.remove('active');});
+    
+    btnPopup.addEventListener('click', ()=> {wrapper.classList.add('active-popup');});
+    
+    iconClose.addEventListener('click', ()=> {wrapper.classList.remove('active-popup');});
+    
+
+});
+=======
+const wrapper = document.querySelector('.wrapper');
+const loginLink = document.querySelector('.loginLink');
+const registerLink = document.querySelector('.registerLink');
+const btnPopup = document.querySelector('.btnLogin-popup');
+const iconClose = document.querySelector('.iconClose');
+
+
+registerLink.addEventListener('click', ()=> {wrapper.classList.add('active');})
+
+loginLink.addEventListener('click', ()=> {wrapper.classList.remove('active');})
+
+btnPopup.addEventListener('click', ()=> {wrapper.classList.add('active-popup');})
+
+iconClose.addEventListener('click', ()=> {wrapper.classList.remove('active-popup');})
+
+>>>>>>> Stashed changes
