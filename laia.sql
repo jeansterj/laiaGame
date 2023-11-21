4bf8dcdfacaea3ff4f5647757321de6c269def4e
PedroLP7 <113777475+PedroLP7@users.noreply.github.com>
2023-11-21 16:08:55 +0100

    Add files via upload

--- laia.sql
+++ laia.sql
@@ -1,58 +1,134 @@
-DROP DATABASE laiaData;
-CREATE DATABASE laiaData;
-USE laiaData;
+-- MySQL dump 10.13  Distrib 8.0.31, for Win64 (x86_64)
+--
+-- Host: localhost    Database: laiadata
+-- ------------------------------------------------------
+-- Server version	8.0.31
 
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
+--
+-- Table structure for table `juegos`
+--
 
+DROP TABLE IF EXISTS `juegos`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `juegos` (
+  `idjuegos` int NOT NULL,
+  `descripcion` varchar(255) NOT NULL,
+  PRIMARY KEY (`idjuegos`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
-CREATE TABLE ROLES (
-idRol int PRIMARY KEY auto_increment,
-rol varchar (255) NOT NULL
+--
+-- Dumping data for table `juegos`
+--
 
-);
-CREATE TABLE  USUARIOS(
-idUsuario int PRIMARY KEY ,
-nombre  varchar(255) NOT NULL,
-nombreUsuario varchar (255) UNIQUE,
-contraseña varchar (255) NOT NULL,
-apellido1 varchar(255) NOT NULL,
+LOCK TABLES `juegos` WRITE;
+/*!40000 ALTER TABLE `juegos` DISABLE KEYS */;
+INSERT INTO `juegos` VALUES (1,'Warcelona'),(2,'Brasil'),(3,'Kenia'),(4,'India');
+/*!40000 ALTER TABLE `juegos` ENABLE KEYS */;
+UNLOCK TABLES;
 
-fechaNacimiento date NOT NULL,
-id_Rol int ,
-FOREIGN KEY (id_Rol) REFERENCES ROLES (idRol)
-);
+--
+-- Table structure for table `puntuacion`
+--
 
+DROP TABLE IF EXISTS `puntuacion`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `puntuacion` (
+  `idUsuario` int DEFAULT NULL,
+  `idjuegos` int DEFAULT NULL,
+  `puntuacion` int DEFAULT NULL,
+  KEY `idUsuario` (`idUsuario`),
+  KEY `idjuegos` (`idjuegos`),
+  CONSTRAINT `puntuacion_ibfk_1` FOREIGN KEY (`idUsuario`) REFERENCES `usuarios` (`idUsuario`),
+  CONSTRAINT `puntuacion_ibfk_2` FOREIGN KEY (`idjuegos`) REFERENCES `juegos` (`idjuegos`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
+--
+-- Dumping data for table `puntuacion`
+--
 
-CREATE TABLE JUEGOS(
-idjuegos int PRIMARY KEY,
-descripcion varchar(255) NOT NULL
-);
+LOCK TABLES `puntuacion` WRITE;
+/*!40000 ALTER TABLE `puntuacion` DISABLE KEYS */;
+/*!40000 ALTER TABLE `puntuacion` ENABLE KEYS */;
+UNLOCK TABLES;
 
-CREATE TABLE PUNTUACION(
+--
+-- Table structure for table `roles`
+--
 
-idUsuario int,
-idjuegos int,
-puntuacion int,
+DROP TABLE IF EXISTS `roles`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `roles` (
+  `idRol` int NOT NULL AUTO_INCREMENT,
+  `rol` varchar(255) NOT NULL,
+  PRIMARY KEY (`idRol`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
-FOREIGN KEY (idUsuario) REFERENCES USUARIOS (idUsuario),
-FOREIGN KEY (idjuegos) REFERENCES JUEGOS (idjuegos)
+--
+-- Dumping data for table `roles`
+--
 
-);
+LOCK TABLES `roles` WRITE;
+/*!40000 ALTER TABLE `roles` DISABLE KEYS */;
+INSERT INTO `roles` VALUES (1,'SUPERADMIN'),(2,'ADMIN'),(3,'JUGADOR');
+/*!40000 ALTER TABLE `roles` ENABLE KEYS */;
+UNLOCK TABLES;
 
--- Insertar el rol de superadmin en la tabla ROLES
-INSERT INTO ROLES ( rol)
-VALUES ('SUPERADMIN'),('ADMIN'),('JUGADOR');
+--
+-- Table structure for table `usuarios`
+--
 
--- Insertar un superadmin en la tabla USUARIOS
-INSERT INTO USUARIOS (idUsuario, nombre, nombreUsuario, contraseña, apellido1, fechaNacimiento,id_Rol)
-VALUES (1, 'WarcelonaWatcher', 'SuperAdminUser', 'superadmin123', 'Apellido1Super', '1990-01-01',1),
-(2, 'Admin', 'AdminUser', 'admin123', 'Apellido1Admin',  '1990-01-01',2)
+DROP TABLE IF EXISTS `usuarios`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `usuarios` (
+  `idUsuario` int NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(255) NOT NULL,
+  `nombreUsuario` varchar(255) DEFAULT NULL,
+  `contrasena` varchar(255) NOT NULL,
+  `apellido1` varchar(255) NOT NULL,
+  `fechaNacimiento` date NOT NULL,
+  `id_Rol` int DEFAULT NULL,
+  PRIMARY KEY (`idUsuario`),
+  UNIQUE KEY `nombreUsuario` (`nombreUsuario`),
+  KEY `id_Rol` (`id_Rol`),
+  CONSTRAINT `usuarios_ibfk_1` FOREIGN KEY (`id_Rol`) REFERENCES `roles` (`idRol`)
+) ENGINE=InnoDB AUTO_INCREMENT=18 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
-;
+--
+-- Dumping data for table `usuarios`
+--
 
-INSERT INTO JUEGOS ( idjuegos,descripcion)
-VALUES (1,'Warcelona'),(2,'Brasil'),(3,'Kenia'),(4,'India');
+LOCK TABLES `usuarios` WRITE;
+/*!40000 ALTER TABLE `usuarios` DISABLE KEYS */;
+INSERT INTO `usuarios` VALUES (1,'WarcelonaWatcher','SuperAdminUser','superadmin123','Apellido1Super','1990-01-01',1),(2,'Admin','AdminUser','admin123','Apellido1Admin','1990-01-01',2),(16,'Pedro','PedroUser','1234','Lopez Perez','1995-08-21',3);
+/*!40000 ALTER TABLE `usuarios` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
-
+-- Dump completed on 2023-11-21 16:08:14
