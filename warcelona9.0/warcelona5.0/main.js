c002e8bed3dad7a39f1f4f4a1f10a231eaf2a558
AriveroM <124346625+AriveroM@users.noreply.github.com>
2023-12-18 08:23:51 +0100

    Add files via upload

--- warcelona9.0/warcelona5.0/main.js
+++ warcelona9.0/warcelona5.0/main.js
@@ -1,6 +1,5 @@
 import { Character } from './character.js';
 import { Projectile } from './projectile.js';
-import { TripleShotProjectile } from './tripleProyectile.js';
 import { EnemySpawner } from './enemySpawner.js';
 import { ChargeEnemy } from './chargeEnemy.js';
 import { Enemy } from './enemy.js';
@@ -25,7 +24,7 @@ let omnishootInterval;
 
 const orbital = new Orbital(15,0.015,60);
 
-const chispas = new ChasingEnemy(50,50);
+const chispas = new ChasingEnemy(-40,-40);
 
 window.addEventListener('keydown', (event) => {
     keysPressed[event.code] = true;
@@ -35,14 +34,7 @@ window.addEventListener('keyup', (event) => {
     keysPressed[event.code] = false;
 });
 
-function startTimer() {
-    let totalSeconds = 0;
-    setInterval(() => {
-        totalSeconds++;
-        const timerElement = document.getElementById('timer');
-        timerElement.textContent = formatTime(totalSeconds);
-    }, 1000);
-}
+
 
 function difficulty()
 { 
@@ -99,12 +91,6 @@ function difficulty()
     }  
 }
 
-function formatTime(totalSeconds) {
-    const minutes = Math.floor(totalSeconds / 60).toString().padStart(2, '0');
-    const seconds = (totalSeconds % 60).toString().padStart(2, '0');   
-    return `${minutes}:${seconds}`;
-}
-
 function setCookie(name, value, days) {
     var expires = "";
     if (days) {
@@ -124,14 +110,31 @@ function setCookie(name, value, days) {
     startScreen.style.display = 'flex';
     startScreen.style.justifyContent = 'center';
     startScreen.style.alignItems = 'center';
+    startScreen.style.flexDirection = 'column'; 
 
+    
+    const titleImage = document.createElement('img');
+    titleImage.src = './images/warcelonaButton.png'; 
+    titleImage.style.width = '50%'; 
+    titleImage.style.position = 'fixed';
+    titleImage.style.top = '5%';
+    titleImage.style.height = 'auto';
+    titleImage.style.opacity = '0'; 
+    titleImage.style.transition = 'opacity 2s ease-in-out'; 
+
+   
     const startButton = document.createElement('button');
     startButton.classList.add('start-button');
     startButton.textContent = 'Start Game';
     startButton.addEventListener('click', startGame);
 
+    
+    startScreen.appendChild(titleImage);
     startScreen.appendChild(startButton);
     document.body.appendChild(startScreen);
+
+    
+    setTimeout(() => { titleImage.style.opacity = '1'; }, 500);
 }
 
 function startGame() {
@@ -145,41 +148,40 @@ function startGame() {
 function showDialogueScreen() {
     const dialogueScreen = document.createElement('div');
     dialogueScreen.id = 'dialogue-screen';
-    dialogueScreen.style.position = 'fixed'; // Usar 'fixed' para cubrir toda la pantalla
+    dialogueScreen.style.position = 'fixed'; 
     dialogueScreen.style.top = '0';
     dialogueScreen.style.left = '0';
     dialogueScreen.style.width = '100%';
     dialogueScreen.style.height = '100%';
-    dialogueScreen.style.backgroundColor = 'rgba(0, 0, 0, 0.5)'; // Fondo negro con transparencia
+    dialogueScreen.style.backgroundColor = 'rgba(0, 0, 0, 0.5)';
     dialogueScreen.style.display = 'flex';
     dialogueScreen.style.flexDirection = 'column';
     dialogueScreen.style.justifyContent = 'center';
     dialogueScreen.style.alignItems = 'center';
-    dialogueScreen.style.zIndex = '1000'; // Asegúrate de que esté por encima de otros elementos
+    dialogueScreen.style.zIndex = '1000'; 
 
-    // Texto del diálogo
+    
     const dialogueText = document.createElement('p');
     dialogueText.id = 'dialogue-text';
-    dialogueText.style.color = 'white'; // Texto blanco para contraste
-    dialogueText.style.fontSize = '24px'; // Tamaño del texto
+    dialogueText.style.color = 'white';
+    dialogueText.style.fontSize = '24px'; 
     dialogueText.style.textAlign = 'center';
     dialogueText.style.padding = '20px';
 
-    // Array de diálogos
-    const dialogues = ["Bienvenido a Warcelona", "Tendras que defender a Laia", "Usaras las teclas W,A,S,D para moverte","Las teclas de dirección del teclado para disparar","Listo?"];
+   
+    const dialogues = ["Bienvenido", "Laia tiene que coger un vuelo", "Pero ha anochecido","Y en la boyante ciudad de Barcelona acechan peligrosos malechores","Laia necesita ayuda, podrias prestarsela?", "En primer lugar Laia tendra que evitar a los malechores usa las teclas de control WASD para ello", "utiliza las flechas de direccion del teclado para incapazitar a los enemigos", "Contribuir a la seguridad en Barcelona ara que Laia obtenga poderes que le permitiran completar su aventura", "Sobrevive al menos hasta que Laia haya obtenido 10 niveles", "Estas listo?"];
     let currentDialogue = 0;
     dialogueText.textContent = dialogues[currentDialogue];
 
     dialogueScreen.appendChild(dialogueText);
 
-    // Evento de clic para cambiar de diálogo
+    
     dialogueScreen.addEventListener('click', () => {
         currentDialogue++;
         if (currentDialogue < dialogues.length) {
             dialogueText.textContent = dialogues[currentDialogue];
         } else {
-            document.body.removeChild(dialogueScreen);
-            startTimer();
+            document.body.removeChild(dialogueScreen);           
             requestAnimationFrame(gameLoop);
         }
     });
@@ -208,15 +210,30 @@ function showGameOverScreen() {
     gameOverText.style.fontSize = '24px';
     gameOverText.style.textAlign = 'center';
     gameOverText.style.padding = '20px';
-    gameOverText.textContent = 'Game Over. Click to restart.';
+    gameOverText.textContent = 'Has perdido Laia no tiene suficiente poder para salvar al mundo.';
+    if(character.level >= 10)
+    {
+        gameOverText.textContent = 'Felicidades, Laia puede proseguir su aventura, Ha canalizado todo su poder para crear un objeto que le servira para mas adelante.';        
+        const victoryImage = document.createElement('img');
+        victoryImage.src = 'images/connector.png'; 
+        victoryImage.style.width = '30%'; 
+        victoryImage.style.height = '30%';
+        victoryImage.style.marginTop = '20px';
 
-    const nextGameButton = document.getElementById('siguientejuego');
+        gameOverScreen.appendChild(victoryImage); 
+    }
+    else
+    {
+        gameOverText.textContent = 'Has perdido Laia no tiene suficiente poder para salvar al mundo.'; 
+    }  
+ 
+    const form = document.getElementById('alfinal'); 
+    form.style.display = 'block';
     const restartButton = document.getElementById('restartGameButton');
-    restartButton.style.display = 'block'; 
-    nextGameButton.style.display = 'block'; 
+    restartButton.style.display = 'block';    
 
     gameOverScreen.appendChild(gameOverText);
-    gameOverScreen.appendChild(nextGameButton);
+    gameOverScreen.appendChild(form);    
     gameOverScreen.appendChild(restartButton);
 
     
